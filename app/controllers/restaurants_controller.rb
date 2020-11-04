b2053ed3fae8a0eede30ef586164fe9fde77bc19
Madalena Boavida Guerra <madalena.boavida@icloud.com>
2020-11-04 15:38:14 +0000

    fixed missing code in restaurants create: missing save before.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,6 +12,12 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    else
+      render :new
+    end
   end
 
   private
